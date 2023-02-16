b249b9106378b64a32917148aaa1373616d7ec89
“dariushbadakhshan” <dariush.badakhshan@gmail.com>
2023-02-16 15:08:23 +0330

    made a userList Styled-component as a wrapper div to render users list

---
+++ src/components/users/UI/UserList.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const UserList = styled.div`
+  margin: 2rem auto;
+  width: 90%;
+  max-width: 40rem;
+
+  & ul {
+    list-style: none;
+    padding: 1rem;
+  }
+
+  & li {
+    border: 1px solid #ccc;
+    margin: 0.5rem 0;
+    padding: 0.5rem;
+  }
+`;
